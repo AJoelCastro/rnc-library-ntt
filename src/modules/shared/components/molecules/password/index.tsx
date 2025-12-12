99cb3b0e0f339377ef553e44c12b771e0f29fccd
AJoelCastro <misthorn33@gmail.com>
2025-12-12 12:27:54 -0500

    fix input password color

--- src/modules/shared/components/molecules/password/index.tsx
+++ src/modules/shared/components/molecules/password/index.tsx
@@ -38,7 +38,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     borderWidth: 1,
-    borderColor: '#ddd',
+    borderColor: '#7D2EFF',
     borderRadius: 8,
     paddingHorizontal: 12,
     backgroundColor: '#fff',
