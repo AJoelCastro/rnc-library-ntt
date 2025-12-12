affef5802cbebf6f867859bc12c301ae1c9bcbbd
AJoelCastro <misthorn33@gmail.com>
2025-12-12 09:11:11 -0500

    new native modules

--- src/NativeNetworkMonitor.ts
+++ src/NativeNetworkMonitor.ts
@@ -1,6 +1,6 @@
 import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
-import type { ConnectionInfo } from '../types';
+import type { ConnectionInfo } from './modules/network_monitor/types';
 
 export interface Spec extends TurboModule {
   getCurrentState(): Promise<ConnectionInfo>;
