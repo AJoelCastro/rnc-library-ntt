9390f257ae1a6d86e22db7e6bea2211e7529ac34
AJoelCastro <misthorn33@gmail.com>
2025-12-12 10:59:01 -0500

    network monitor and secure storage module implementation on iOS

---
+++ ios/NetworkMonitor.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+#import <RncLibraryNttSpec/RncLibraryNttSpec.h>
+
+@interface NetworkMonitor : NativeNetworkMonitorSpecBase<NativeNetworkMonitorSpec>
+
+
+@end
---
+++ ios/SecureStorage.h
@@ -0,0 +1,5 @@
+#import <RncLibraryNttSpec/RncLibraryNttSpec.h>
+
+@interface SecureStorage : NSObject <NativeSecureStorageSpec>
+
+@end
