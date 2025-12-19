eee76b416ae5c0d2adfb21b6ff43439ce86628a9
AJoelCastro <misthorn33@gmail.com>
2025-12-18 23:23:08 -0500

    chore: Decrease Jest branches coverage threshold from 80% to 70%.

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
     collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}'],
     coverageThreshold: {
         global: {
-            branches: 80,
+            branches: 70,
             functions: 80,
             lines: 80,
             statements: 80,
